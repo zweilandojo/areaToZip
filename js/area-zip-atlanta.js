27b563327a771d6088a926a77d6e8d0e73661119
Zak Weiland <zweiland@Zaks-MacBook-Pro.local>
2018-08-06 14:33:26 -0500

    moved files

---
+++ js/area-zip-atlanta.js
@@ -0,0 +1,27 @@
+const atlanta = [
+    {area: 'All', zips: '30002, 30021, 30030, 30033, 30079, 30084, 30305, 30306, 30307, 30308, 30309, 30312, 30316, 30317, 30319, 30324, 30326, 30329, 30338, 30340, 30341, 30342, 30345, 30360'},
+    {area: 'Avondale Estates', zips: '30002'},
+    {area: 'Clarkston', zips: '30021'},
+    {area: 'City of Decatur', zips: '30030'},
+    {area: 'North Decatur', zips: '30002'},
+    {area: 'Scottdale / North Avondale', zips: '30079'},
+    {area: 'Tucker', zips: '30084'},
+    {area: 'Peachtree Hills', zips: '30305'},
+    {area: 'Druid Hills / Virginia Highlands / Emory', zips: '30306'},
+    {area: 'Druid Hills / Emory Univ. / Candler Park / Lake Claire', zips: '30307'},
+    {area: 'Midtown / Old Fourth Ward / Piedmont Park South', zips: '30308'},
+    {area: 'Ansley Park / Sherwood Forest / Brookwood', zips: '30309'},
+    {area: 'Grant Park', zips: '30312'},
+    {area: 'East Atlanta / Cabbagetown / Reynoldstown', zips: '30316'},
+    {area: 'Eastlake / Kirkwood / Edgewood', zips: '30317'},
+    {area: 'Brookhaven / Oglethorpe / Ashford', zips: '30319'},
+    {area: 'Morningside / Lenox Park / Lindbergh', zips: '30324'},
+    {area: 'Lenox Square', zips: '30326'},
+    {area: 'Emory University / CDC / Clifton Corridor', zips: '30329'},
+    {area: 'Dunwoody', zips: '30338'},
+    {area: 'Doraville / Northlake / Embry Hills', zips: '30340'}
+    {area: 'Chamblee / Northlake / Century Center', zips: '30341'},
+    {area: 'Buckhead / Roswell Road / Sandy Springs South', zips: '30342'},
+    {area: 'Northlake / Oak Grove', zips: '30345'},
+    {area: 'Dunwoody / Spalding Lakes', zips: '30360'}
+];
